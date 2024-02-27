0c513e87dda33a57cd1d095f5c9910f91ded85f6
modiox <moodyalyalak@gmail.com>
2024-02-27 20:49:52 +0300

    to-do-api updated

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 // Fetch Todos
-fetch('https://jsonplaceholder.typicode.com/todos')
-.then(response => response.json())
-.then(data => {
-    const todosGrid = document.getElementById('todos-grid');
-    data.forEach(todo => {
-        const todoItem = document.createElement('div');
-        todoItem.classList.add('todo');
-        todoItem.textContent = todo.title;
-        todosGrid.appendChild(todoItem);
-    });
-});
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => response.json())
+  .then((data) => {
+    const todosGrid = document.getElementById("todos-grid");
+    data.forEach((todo) => {
+      const todoItem = document.createElement("div");
+      todoItem.classList.add("todo");
 
-// Digital Clock
-function updateClock() {
-const now = new Date();
-const clock = document.getElementById('clock');
-clock.textContent = now.toLocaleTimeString();
-}
-setInterval(updateClock, 1000);
-updateClock();
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.classList.add("todo-checkbox");
+      checkbox.addEventListener("change", function () {
+        if (checkbox.checked) {
+          title.style.textDecoration = "line-through";
+        } else {
+          title.style.textDecoration = "none";
+        }
+      });
+
+      const title = document.createElement("span");
+      title.textContent = todo.title;
+      todoItem.appendChild(checkbox);
+      todoItem.appendChild(title);
+
+      todosGrid.appendChild(todoItem);
+    });
+  });
